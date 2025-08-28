a6f1ce45e438b77753922bf6a34c00ba552a7009
Kevinho71 <kevin.guzman@ucb.edu.bo>
2025-08-28 16:28:32 -0400

    Creacion del proyecto

---
+++ src/main/java/com/perfumeria/aquadebelen/aquadebelen/AquadebelenApplication.java
@@ -0,0 +1,13 @@
+package com.perfumeria.aquadebelen.aquadebelen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquadebelenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquadebelenApplication.class, args);
+	}
+
+}
